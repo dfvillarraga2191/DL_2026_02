9ae3452e78fab357371dd34d5d7431a083923046
Daniel Villarraga <danielvillarraga@Daniels-MacBook-Air-56.local>
2026-09-14 10:57:55 -0500

    modified ignores and added ipynbs

---
+++ Semana 7/utils.py
@@ -0,0 +1,39 @@
+import ipywidgets as widgets
+from IPython.display import display
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+
+# Nombres de las clases del dataset COCO, en el orden contiguo 0-79 que usan los
+# archivos de anotación estilo YOLO de las muestras coco8
+COCO_CLASSES = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck",
+    "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+    "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra",
+    "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+    "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+    "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange",
+    "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+    "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink",
+    "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier",
+    "toothbrush",
+]
+
+# Las 17 articulaciones anotadas en los puntos clave de COCO, y los pares de
+# articulaciones que se unen al dibujar el esqueleto
+COCO_KEYPOINT_NAMES = [
+    "nose", "left_eye", "right_eye", "left_ear", "right_ear",
+    "left_shoulder", "right_shoulder", "left_elbow", "right_elbow",
+    "left_wrist", "right_wrist", "left_hip", "right_hip",
+    "left_knee", "right_knee", "left_ankle", "right_ankle",
+]
+
+COCO_SKELETON = [
+    (0, 1), (0, 2), (1, 3), (2, 4),           # cabeza
+    (5, 6), (5, 7), (7, 9), (6, 8), (8, 10),  # brazos y hombros
+    (5, 11), (6, 12), (11, 12),               # torso
+    (11, 13), (13, 15), (12, 14), (14, 16),   # piernas
+]
+
